3f6f70344e049acf310496e7ea3029b051a9fea8
vplotnikovcode <viper22121993@gmail.com>
2023-05-22 09:56:24 +0300

    config update

--- vkinder/config.py
+++ vkinder/config.py
@@ -1,10 +1,6 @@
-# TOKEN_USER = 'vk1.a.-IbRwk4CJ07F3FENNQYHwTk8CI7gwO-UVqmeDiqLAUsiwIBzvYFS8hCIx-UeO71yLxzpw3E2V0t9AbylyWvry0M19Sooxm' \
-#              'ULQrQLT2ka2DGxedXHYfwYNxYpS6QvoXRv7DHboLwPwK6Ejc8K3obfi__02Hw3x945pr3nhGTWHcZbqHY5s7H4UfHL4VCwl13bH4' \
-#              'NO7o_vckxAOvel-HBb9w'
-
-TOKEN_GROUP = 'vk1.a.POou64ZXcc7pZXsWjTAs7lC4RT_QTNGzrZ6cMz8D61WuIh0aHE5XiZ2fxFWdBHQ_bHe1HzZjY-PKksjECy-eWmeqr4JZbPcTZYh0Mne_WQj175tj9bHjrjlniC7EJlX5VCdhDUWs5WCshZUWKKjeazbdQYmJImkKVl_bHhZlyDdGm7gNOY5pFd67dNwEa2JPxlTSA5VCgaMeUZNgAYiHRw'
-
-#TOKEN_GROUP = "vk1.a.T-KXfnH1TkViemgd55dadbNrXDS1Sm6GaWU3y8F9D-ti0enHGthbrJp_tUnC5AzfbOhaMube22zJJEzJMBqDWnlg3vuw2QXx1jyzTqB_0yqQmMbfBH3HEky6xVmSl63VFWpccXjNmk5sN_GK-81Cc8KlmtPwDpvF-IMW294s7mJ89idEEHzhL6YOTLyU-r-u3tb4eyrHpSzFGadD9qKtBA"
+TOKEN_GROUP = "vk1.a.T-KXfnH1TkViemgd55dadbNrXDS1Sm6GaWU3y8F9D-ti0enHGthbrJp_tUnC5AzfbOhaMube22zJJEzJMBqDWnlg3vuw2" \
+	     "QXx1jyzTqB_0yqQmMbfBH3HEky6xVmSl63VFWpccXjNmk5sN_GK-81Cc8KlmtPwDpvF-IMW294s7mJ89idEEHzhL6YOTLyU-r-u3" \
+	     "tb4eyrHpSzFGadD9qKtBA"
 TOKEN_USER = "vk1.a.XSiborj1TwSgwzix2CHliwRUQXth8xV385U55aTGesEtmzVNNUgnotqBzKHyusvnMUf6LByeysf5yilT07fVdEBqGtNCkY" \
              "j_yWr_nanVQY4qMP8u2mgjVeNolgbRijet3XVtJ-jyhm3pMwrVHe_o6yKxgO8kbG6N7LEAB6EqCLyr0GAyv3WTxQQSLnc6yG5veA" \
              "ldsBd0tdcgAE1ay_szOg"
